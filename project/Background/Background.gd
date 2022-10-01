8a28250e81a7acc1d69f4d537483f34b160f3401
wendallR <wendallar125@gmail.com>
2022-10-01 16:13:43 -0400

    Add decorative background
    The background has a set of stars that randomly changes per loading of the scene

---
+++ project/Background/Background.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+var _star_count := 40
+
+
+onready var _star := load("res://Background/Star/Star.tscn")
+
+
+func _ready():
+	randomize()
+	for i in _star_count:
+		#Instances do not have set types so type cannot be inferred
+		var new_star = _star.instance()
+		add_child(new_star)
+		var random_position_x := rand_range(0, 1024)
+		var random_position_y := rand_range(0, 600)
+		new_star.position = Vector2(random_position_x, random_position_y)
