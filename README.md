f817cc7c97034050c52861d5d71c41e2113b84ea
wendallR <wendallar125@gmail.com>
2022-09-30 21:10:49 -0400

    Add reload timer
    The reload timer limits how fast the player can shoot

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 
 # Ludum Dare Game
 - My Jam Game for Ludum Dare 51
-
+- Made using Godot 3.5.1, and Paint.net
+- Play the latest build [here]()
 
 ## Features and Controls
-- 
+- A and D to move left and right
+- Space to fire
+
+# Goals For the Jam
+
+- [X] Movement
+- [X] Shooting a bullet
+- [ ] Enemy Bullets Kill the player
+- [ ] Enemys that spawn and shoot the player
+- [ ] Score
+- [ ] Basic Hud
+- [ ] Super Attacks
+- [ ] Super Attack recharges every 10 seconds **Required for the Compo Theme**
+- [ ] Finished game Title, Gameplay, and End screen
+- [ ] Ability to Return to the Title after the End
+- [ ] Sound effects
+- [ ] Music
+- [ ] Juice as particle effects
--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -6,16 +6,31 @@ signal bullet_fired(id, fired_from)
 
 var _velocity := Vector2.ZERO
 var _speed := 500
-var bullet_id := 1
+var _bullet_id := 1
+var _has_fired := false
+
+
+onready var _reload_timer := find_node("ReloadTimer")
+
+
+func _ready():
+# warning-ignore:return_value_discarded
+	_reload_timer.connect("timeout", self, "_on_reload_timer_timeout")
 
 
 func _physics_process(_delta: float) -> void:
 	var x_movement := _get_x_input()
 	_velocity = move_and_slide(Vector2(x_movement, 0) * _speed, Vector2.UP)
 	
-	if Input.is_action_just_pressed("shoot"):
-		emit_signal("bullet_fired", bullet_id, self)
+	if Input.is_action_just_pressed("shoot") and not _has_fired:
+		emit_signal("bullet_fired", _bullet_id, self)
+		_has_fired = true
+		_reload_timer.start()
 
 
 func _get_x_input() -> float:
 	return Input.get_axis("move_left", "move_right")
+
+
+func _on_reload_timer_timeout():
+	_has_fired = false
